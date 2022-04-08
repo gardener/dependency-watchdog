99661de49361afde1b8343d1f824300ec09f4d6c
Rishabh Patel <rishabh.patel@sap.com>
2022-04-08 16:38:35 +0530

    removed createScalesGetter from probercmd

--- cmd/probercmd.go
+++ cmd/probercmd.go
@@ -4,19 +4,15 @@ import (
 	"context"
 	"flag"
 	"fmt"
+
 	"github.com/gardener/dependency-watchdog/controllers"
 	"github.com/gardener/dependency-watchdog/internal/prober"
+	"github.com/gardener/dependency-watchdog/internal/util"
 	gardenextensions "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/client-go/discovery/cached/memory"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/restmapper"
-	"k8s.io/client-go/scale"
 	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -95,9 +91,9 @@ func startProberControllerMgr(ctx context.Context, args []string, logger logr.Lo
 		return nil, fmt.Errorf("failed to start the prober controller manager %w", err)
 	}
 
-	scalesGetter, err := createScalesGetter(ctrl.GetConfigOrDie())
+	scalesGetter, err := util.CreateScalesGetter(ctrl.GetConfigOrDie())
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create clientSet for scalesGetter %w", err)
 	}
 	if err := (&controllers.ClusterReconciler{
 		Client:      mgr.GetClient(),
@@ -109,15 +105,4 @@ func startProberControllerMgr(ctx context.Context, args []string, logger logr.Lo
 		return nil, fmt.Errorf("failed to register cluster reconciler with the prober controller manager %w", err)
 	}
 	return mgr, nil
-}
-
-func createScalesGetter(config *rest.Config) (scale.ScalesGetter, error) {
-	clientSet, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create clientSet for scalesGetter %w", err)
-	}
-	mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(clientSet.Discovery()))
-	scaleKindResolver := scale.NewDiscoveryScaleKindResolver(clientSet.Discovery())
-	scalesGetter := scale.New(clientSet.RESTClient(), mapper, dynamic.LegacyAPIPathResolverFunc, scaleKindResolver)
-	return scalesGetter, nil
 }
