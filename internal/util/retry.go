e18effe35e6da06b52c864fab32b4f21dcc919ff
Rishabh Patel <rishabh.patel@sap.com>
2022-04-08 16:44:29 +0530

    return result if error is nil

--- internal/util/retry.go
+++ internal/util/retry.go
@@ -21,18 +21,19 @@ func Retry[T any](ctx context.Context, operation string, fn func() (T, error), n
 		default:
 		}
 		result, err = fn()
-		if err != nil {
-			if !canRetry(err) {
-				logger.Error(err, "exiting retry as canRetry has returned false", "operation", operation, "exitOnAttempt", i)
-				return RetryResult[T]{Err: err}
-			}
-			select {
-			case <-ctx.Done():
-				logger.Error(ctx.Err(), "context has been cancelled, stopping retry", "operation", operation)
-				return RetryResult[T]{Err: ctx.Err()}
-			case <-time.After(backOff):
-				logger.V(4).Info("will attempt to retry operation", "operation", operation, "currentAttempt", i, "error", err)
-			}
+		if err == nil {
+			return RetryResult[T]{Value: result, Err: err}
+		}
+		if !canRetry(err) {
+			logger.Error(err, "exiting retry as canRetry has returned false", "operation", operation, "exitOnAttempt", i)
+			return RetryResult[T]{Err: err}
+		}
+		select {
+		case <-ctx.Done():
+			logger.Error(ctx.Err(), "context has been cancelled, stopping retry", "operation", operation)
+			return RetryResult[T]{Err: ctx.Err()}
+		case <-time.After(backOff):
+			logger.V(4).Info("will attempt to retry operation", "operation", operation, "currentAttempt", i, "error", err)
 		}
 	}
 	return RetryResult[T]{Value: result, Err: err}
