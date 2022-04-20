5ce98a2a2b53ef079baea5a1ffd491d3ca452a29
Rishabh Patel <rishabh.patel@sap.com>
2022-04-20 12:31:43 +0530

    changed probestatus tests to use gomega

--- internal/prober/probestatus_test.go
+++ internal/prober/probestatus_test.go
@@ -3,27 +3,24 @@ package prober
 import (
 	"errors"
 	"fmt"
-	. "github.com/onsi/gomega"
 	"testing"
 	"time"
+
+	. "github.com/onsi/gomega"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 func TestIsHealthy(t *testing.T) {
+	g := NewWithT(t)
 	unhealthy := createProbeStatus(0, 0, nil, nil)
 	healthy := createProbeStatus(4, 0, nil, nil)
 	successThreshold := 3
 
-	if unhealthy.isHealthy(successThreshold) {
-		t.Fatalf("IsHealthy failed, Expected false for unhealthy status, but got true")
-	}
-	if !healthy.isHealthy(successThreshold) {
-		t.Fatalf("IsHealthy failed, Expected true for healthy status, but got false")
-	} else {
-		t.Log("IsHealthy passed")
-	}
+	g.Expect(unhealthy.isHealthy(successThreshold)).To(BeFalse(), "unhealthy.isHealthy should have been false")
+	g.Expect(healthy.isHealthy(successThreshold)).To(BeTrue(), "healthy.isHealthy should have been true")
+	t.Log("IsHealthy passed")
 }
 
 func TestIsUnhealthy(t *testing.T) {
@@ -31,96 +28,79 @@ func TestIsUnhealthy(t *testing.T) {
 	unhealthy := createProbeStatus(0, 4, nil, nil)
 	healthy := createProbeStatus(0, 2, nil, nil)
 	failureThreshold := 3
-	g.Expect(unhealthy.isUnhealthy(failureThreshold)).To(BeFalse(), fmt.Sprintf("unhealthy.isUnhealthy should have been true"))
-	g.Expect(healthy.isUnhealthy(failureThreshold)).To(BeFalse())
+
+	g.Expect(unhealthy.isUnhealthy(failureThreshold)).To(BeTrue(), "unhealthy.isUnhealthy should have been true")
+	g.Expect(healthy.isUnhealthy(failureThreshold)).To(BeFalse(), "healthy.isUnhealthy should have been false")
+	t.Log("IsUnhealthy Passed")
 }
 
 func TestRestBackoff(t *testing.T) {
+	g := NewWithT(t)
 	ps := createProbeStatus(0, 0, nil, time.NewTimer(1*time.Minute))
 	prevTimer := ps.backOff
 	ps.resetBackoff(1 * time.Millisecond)
-	if prevTimer.Stop() {
-		t.Fatalf("ResetBackoff failed, Did not stop the existing timer before starting a new one")
-	}
+	g.Expect(prevTimer.Stop()).To(BeFalse(), "RestBackOff should have stopped the existing timer before starting a new one")
 
 	ps = createProbeStatus(0, 0, nil, nil)
 	ps.resetBackoff(1 * time.Millisecond)
-	if ps.backOff == nil {
-		t.Fatalf("ResetBackoff failed, Did not start a new timer for the case where probestatus backoff is nil")
-	}
+	g.Expect(ps.backOff).ToNot(BeNil(), "RestBackOff should start a new timer if probestatus backOff is nil")
 
 	ps.backOff.Stop()
 	t.Log("ResetBackoff Passed")
 }
 
 func TestRecordSuccess(t *testing.T) {
+	g := NewWithT(t)
 	ps := createProbeStatus(2, 0, nil, time.NewTimer(1*time.Minute))
 	successThreshold := 3
 	successCount := ps.successCount
 
 	ps.recordSuccess(successThreshold)
-	if ps.successCount-successCount != 1 {
-		t.Fatalf("RecordSuccess failed, Expected success count %v but got %v", successCount+1, ps.successCount)
-	}
-	if ps.errorCount != 0 {
-		t.Fatalf("RecordSuccess failed, Expected error count %v but got %v", 0, ps.errorCount)
-	}
-	if ps.lastErr != nil {
-		t.Fatalf("RecordSuccess failed, Expected lastErr to be nil but go %v", ps.lastErr)
-	}
+
+	g.Expect(ps.successCount).To(Equal(successCount+1), "RecordSuccess should have incremented success count by 1")
+	g.Expect(ps.errorCount).To(BeZero(), "RecordSuccess should have made errorCount equal to 0")
+	g.Expect(ps.lastErr).To(BeNil(), "RecordSuccess should have made lastErr equal to nil")
+
 	t.Log("RecordSuccess Passed")
 }
 
 func TestRecordFailure(t *testing.T) {
+	g := NewWithT(t)
 	ps := createProbeStatus(0, 1, nil, nil)
 	failureThreshold := 3
 	errCount := ps.errorCount
 	err := errors.New("failure")
 
 	ps.recordFailure(err, failureThreshold, 0)
-	if ps.errorCount != errCount+1 {
-		t.Fatalf("RecordFailure failed, Expected errorCount %v, but got %v", errCount+1, ps.errorCount)
-	}
-	if ps.backOff != nil {
-		t.Fatalf("RecordFailure failed, Expected backOff to be nil but go %v", ps.backOff)
-	}
-	if ps.successCount != 0 {
-		t.Fatalf("RecordFailure failed, Expected successCount to be %v but got %v", 0, ps.successCount)
-	}
-	if ps.lastErr != err {
-		t.Fatalf("RecordFailure failed, Expected last error to be %v but got %v", err, ps.lastErr)
-	}
+
+	g.Expect(ps.errorCount).To(Equal(errCount+1), "RecordFailure should have incremented the errorCount by 1")
+	g.Expect(ps.backOff).To(BeNil(), "RecordFailure should not reset backOff if failureThreshold is not crossed")
+	g.Expect(ps.successCount).To(BeZero(), "RecordFailure should have made successCount equal to 0")
+	g.Expect(ps.lastErr).ToNot(BeNil(), "RecordFailure should set the lastErr value to the current error")
 
 	ps.recordFailure(err, failureThreshold, 1*time.Minute)
-	if ps.backOff == nil {
-		t.Fatalf("RecordFailure failed, Got nil backOff, expected a non nil value")
-	}
+
+	g.Expect(ps.backOff).ToNot(BeNil(), "RecordFailure should have reset the backOff when failureThreshold is crossed")
 
 	ps.backOff.Stop()
 	t.Log("RecordFailure Passed")
 }
 
 func TestCanIgnoreProbeError(t *testing.T) {
+	g := NewWithT(t)
 	ps := createProbeStatus(0, 0, nil, nil)
 	err := errors.New("test")
-	if ps.canIgnoreProbeError(err) {
-		t.Fatalf("CanIgnoreProbeError failed, Expected false for error %v but got true", err)
-	}
-	if !ps.canIgnoreProbeError(apierrors.NewNotFound(schema.GroupResource{}, "test")) {
-		t.Fatalf("CanIgnoreProbeError failed, Expected true for a notFound error but got false")
-	}
-	if !ps.canIgnoreProbeError(apierrors.NewForbidden(schema.GroupResource{}, "test", errors.New("forbidden"))) {
-		t.Fatalf("CanIgnoreProbeError failed, Expected true for a forbidden request error but got false")
-	}
-	if !ps.canIgnoreProbeError(apierrors.NewUnauthorized("unauthorized")) {
-		t.Fatalf("CanIgnoreProbeError failed, Expected true for an unauthorized request error but got false")
-	}
-	if !ps.canIgnoreProbeError(apierrors.NewTooManyRequests("Too many requests", 10)) {
-		t.Fatalf("CanIgnoreProbeError failed, Expected true for too many requests error type but got false")
-	}
-	if ps.backOff == nil {
-		t.Fatalf("CanIgnoreProbeError failed, Expected backOff to be non-nil but got nil")
-	}
+
+	g.Expect(ps.canIgnoreProbeError(err)).To(BeFalse(), fmt.Sprintf("CanIgnoreProbeError should return false for %v", err))
+	g.Expect(ps.canIgnoreProbeError(apierrors.NewNotFound(schema.GroupResource{}, "test"))).To(BeTrue(),
+		"CanIgnoreProbeError should return true for a NotFound error")
+	g.Expect(ps.canIgnoreProbeError(apierrors.NewForbidden(schema.GroupResource{}, "test", errors.New("forbidden")))).To(BeTrue(),
+		"CanIgnoreProbeError should return true for a Forbidden request error")
+	g.Expect(ps.canIgnoreProbeError(apierrors.NewUnauthorized("unauthorized"))).To(BeTrue(),
+		"CanIgnoreProbeError should return true for an Unauthorized request error")
+	g.Expect(ps.canIgnoreProbeError(apierrors.NewTooManyRequests("Too many requests", 10))).To(BeTrue(),
+		"CanIgnoreProbeError should return true for a TooManyRequests error")
+	g.Expect(ps.backOff).ToNot(BeNil(), "CanIgnoreProbeError should reset backOff in case of TooManyRequests error")
 
 	ps.backOff.Stop()
 	t.Log("CanIgnoreProbeError Passed")
