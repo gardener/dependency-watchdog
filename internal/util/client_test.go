d3c1564371af0599278b8e02b55b58bca55adfb2
Madhav Bhargava <madhav.bhargava@sap.com>
2022-04-22 16:45:03 +0530

    removed ginkgo suite files and added template to create testsuite

--- internal/util/client_test.go
+++ internal/util/client_test.go
@@ -5,24 +5,18 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-	"testing"
-
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
-
 	. "github.com/onsi/gomega"
-	"github.com/stretchr/testify/suite"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	//apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/yaml"
+	"log"
+	"os"
+	"path/filepath"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"testing"
 )
 
 var (
@@ -32,163 +26,201 @@ var (
 	ctx            context.Context
 	deployment     appsv1.Deployment
 	secret         corev1.Secret
+	k8sClient      client.Client
 )
 
-type ClientTestSuite struct {
-	suite.Suite
-	testEnv   *envtest.Environment
-	k8sClient client.Client
-	cfg       *rest.Config
+func BeforeSuite(t *testing.T) {
+	t.Log("running before suite")
+	//fileExistsOrFail(secretPath)
+	//fileExistsOrFail(deploymentPath)
+	//fileExistsOrFail(kubeConfigPath)
 }
 
-func (s *ClientTestSuite) SetupSuite() {
-	var err error
-	log.Println("setting up testEnv")
-	fileExistsOrFail(secretPath)
-	fileExistsOrFail(deploymentPath)
-	fileExistsOrFail(kubeConfigPath)
-	s.testEnv = &envtest.Environment{}
-	s.cfg, err = s.testEnv.Start()
-	if err != nil {
-		log.Fatalf("error in starting testEnv: %v", err)
+func AfterSuite(t *testing.T) {
+	t.Log("running after suite")
+}
+
+func TestSuite(t *testing.T) {
+	tests := []struct {
+		title string
+		run   func(t *testing.T)
+	}{
+		{"test if secret is not found", testSecretNotFound},
 	}
-	if s.cfg == nil {
-		log.Fatalf("Got nil config from testEnv")
+	BeforeSuite(t)
+	for _, test := range tests {
+		t.Run(test.title, func(t *testing.T) {
+			test.run(t)
+		})
 	}
-	s.k8sClient, err = client.New(s.cfg, client.Options{Scheme: scheme.Scheme})
-	if err != nil {
-		log.Fatalf("error in creating new client: %v", err)
-	}
-	if s.k8sClient == nil {
-		log.Fatalf("Got a nil k8sClient")
-	}
+	AfterSuite(t)
 }
 
-func (s *ClientTestSuite) TearDownSuite() {
-	log.Println("tearing down envTest")
-	err := s.testEnv.Stop()
-	if err != nil {
-		log.Fatalf("error in stopping testEnv: %v", err)
-	}
+func testSecretNotFound(t *testing.T) {
+	//g := NewWithT(t)
+	//_ = setupGetKubeconfigTest(t, k8sClient)
+	//kubeconfig, err := GetKubeConfigFromSecret(ctx, secret.ObjectMeta.Namespace, secret.ObjectMeta.Name, s.k8sClient)
+	//g.Expect(apierrors.IsNotFound(err)).Should(BeTrue())
+	//g.Expect(kubeconfig).Should(BeNil())
+	t.Log("running testSecretNotFound")
 }
 
-func TestClientTestSuite(t *testing.T) {
-	suite.Run(t, new(ClientTestSuite))
-}
-
-func (s *ClientTestSuite) TestSecretNotFound(t *testing.T) {
-	g := NewWithT(t)
-	_ = setupGetKubeconfigTest(t, s.k8sClient)
-	kubeconfig, err := GetKubeConfigFromSecret(ctx, secret.ObjectMeta.Namespace, secret.ObjectMeta.Name, s.k8sClient)
-	g.Expect(apierrors.IsNotFound(err)).Should(BeTrue())
-	g.Expect(kubeconfig).Should(BeNil())
-}
-
-func (s *ClientTestSuite) TestKubeConfigNotFound(t *testing.T) {
-	g := NewWithT(t)
-	cleanup := setupGetKubeconfigTest(t, s.k8sClient)
-	defer cleanup()
-	err := s.k8sClient.Create(ctx, &secret)
-	g.Expect(err).Should(BeNil())
-	kubeconfig, err := GetKubeConfigFromSecret(ctx, secret.ObjectMeta.Namespace, secret.ObjectMeta.Name, s.k8sClient)
-	g.Expect(kubeconfig).Should(BeNil())
-	g.Expect(err).ShouldNot(BeNil())
-	g.Expect(apierrors.IsNotFound(err)).Should(BeFalse())
-}
-
-func (s *ClientTestSuite) TestExtractKubeConfigFromSecret(t *testing.T) {
-	g := NewWithT(t)
-	cleanup := setupGetKubeconfigTest(t, s.k8sClient)
-	defer cleanup()
-	kubeconfigBuffer, err := readFile(kubeConfigPath)
-	g.Expect(err).Should(BeNil())
-	kubeconfig := kubeconfigBuffer.Bytes()
-	g.Expect(kubeconfig).ShouldNot(BeNil())
-
-	secret.Data = map[string][]byte{
-		"kubeconfig": kubeconfig,
-	}
-	err = s.k8sClient.Create(ctx, &secret)
-	g.Expect(err).Should(BeNil())
-
-	actualKubeconfig, err := GetKubeConfigFromSecret(ctx, secret.ObjectMeta.Namespace, secret.ObjectMeta.Name, s.k8sClient)
-	g.Expect(err).Should(BeNil())
-	g.Expect(actualKubeconfig).Should(Equal(kubeconfig))
-}
-
-func (s *ClientTestSuite) TestDeploymentNotFound(t *testing.T) {
-	g := NewWithT(t)
-	setupGetDeploymentTest(t)
-	actual, err := GetDeploymentFor(ctx, deployment.ObjectMeta.Namespace, deployment.ObjectMeta.Name, s.k8sClient)
-	g.Expect(apierrors.IsNotFound(err)).Should(BeTrue())
-	g.Expect(actual).Should(BeNil())
-}
-
-func (s *ClientTestSuite) TestFoundDeployment(t *testing.T) {
-	g := NewWithT(t)
-	setupGetDeploymentTest(t)
-
-	err := s.k8sClient.Create(ctx, &deployment)
-	g.Expect(err).Should(BeNil())
-
-	actual, err := GetDeploymentFor(ctx, deployment.ObjectMeta.Namespace, deployment.ObjectMeta.Name, s.k8sClient)
-	g.Expect(err).Should(BeNil())
-	g.Expect(actual).ShouldNot(BeNil())
-	g.Expect(actual.ObjectMeta.Name).Should(Equal(deployment.ObjectMeta.Name))
-	g.Expect(actual.ObjectMeta.Namespace).Should(Equal(deployment.ObjectMeta.Namespace))
-
-	err = s.k8sClient.Delete(ctx, &deployment)
-	g.Expect(err).Should(BeNil())
-}
-
-func (s *ClientTestSuite) TestCreateScalesGetter(t *testing.T) {
-	g := NewWithT(t)
-	scaleGetter, err := CreateScalesGetter(s.cfg)
-	g.Expect(err).Should(BeNil())
-	g.Expect(scaleGetter).ShouldNot(BeNil())
-}
-
-func (s *ClientTestSuite) TestCreateClientFromKubeconfigBytes(t *testing.T) {
-	g := NewWithT(t)
-	kubeconfigBuffer, err := readFile(kubeConfigPath)
-	g.Expect(err).Should(BeNil())
-	kubeconfig := kubeconfigBuffer.Bytes()
-	g.Expect(kubeconfig).ShouldNot(BeNil())
-
-	cfg, err := CreateClientFromKubeConfigBytes(kubeconfig)
-	g.Expect(err).Should(BeNil())
-	g.Expect(cfg).ShouldNot(BeNil())
-}
-
-// func setupEnvSuite() func() {
-// 	log.Println("setting up testEnv")
-// 	fileExistsOrFail(secretPath)
-// 	fileExistsOrFail(deploymentPath)
-// 	fileExistsOrFail(kubeConfigPath)
-// 	testEnv = &envtest.Environment{}
-// 	cfg, err = testEnv.Start()
-// 	if err != nil {
-// 		log.Fatalf("error in starting testEnv: %v", err)
-// 	}
-// 	if cfg == nil {
-// 		log.Fatalf("Got nil config from testEnv")
-// 	}
-// 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-// 	if err != nil {
-// 		log.Fatalf("error in creating new client: %v", err)
-// 	}
-// 	if k8sClient == nil {
-// 		log.Fatalf("Got a nil k8sClient")
-// 	}
-// 	return func() {
-// 		log.Println("tearing down envTest")
-// 		err := testEnv.Stop()
-// 		if err != nil {
-// 			log.Fatalf("error in stopping testEnv: %v", err)
-// 		}
-// 	}
-// }
-
+//
+//type ClientTestSuite struct {
+//	suite.Suite
+//	testEnv   *envtest.Environment
+//	k8sClient client.Client
+//	cfg       *rest.Config
+//}
+//
+//func (s *ClientTestSuite) SetupSuite() {
+//	var err error
+//	log.Println("setting up testEnv")
+//	fileExistsOrFail(secretPath)
+//	fileExistsOrFail(deploymentPath)
+//	fileExistsOrFail(kubeConfigPath)
+//	s.testEnv = &envtest.Environment{}
+//	s.cfg, err = s.testEnv.Start()
+//	if err != nil {
+//		log.Fatalf("error in starting testEnv: %v", err)
+//	}
+//	if s.cfg == nil {
+//		log.Fatalf("Got nil config from testEnv")
+//	}
+//	s.k8sClient, err = client.New(s.cfg, client.Options{Scheme: scheme.Scheme})
+//	if err != nil {
+//		log.Fatalf("error in creating new client: %v", err)
+//	}
+//	if s.k8sClient == nil {
+//		log.Fatalf("Got a nil k8sClient")
+//	}
+//}
+//
+//func (s *ClientTestSuite) TearDownSuite() {
+//	log.Println("tearing down envTest")
+//	err := s.testEnv.Stop()
+//	if err != nil {
+//		log.Fatalf("error in stopping testEnv: %v", err)
+//	}
+//}
+//
+//func TestClientTestSuite(t *testing.T) {
+//	suite.Run(t, new(ClientTestSuite))
+//}
+//
+//func (s *ClientTestSuite) testSecretNotFound(t *testing.T) {
+//	g := NewWithT(t)
+//	_ = setupGetKubeconfigTest(t, s.k8sClient)
+//	kubeconfig, err := GetKubeConfigFromSecret(ctx, secret.ObjectMeta.Namespace, secret.ObjectMeta.Name, s.k8sClient)
+//	g.Expect(apierrors.IsNotFound(err)).Should(BeTrue())
+//	g.Expect(kubeconfig).Should(BeNil())
+//}
+//
+//func (s *ClientTestSuite) TestKubeConfigNotFound(t *testing.T) {
+//	g := NewWithT(t)
+//	cleanup := setupGetKubeconfigTest(t, s.k8sClient)
+//	defer cleanup()
+//	err := s.k8sClient.Create(ctx, &secret)
+//	g.Expect(err).Should(BeNil())
+//	kubeconfig, err := GetKubeConfigFromSecret(ctx, secret.ObjectMeta.Namespace, secret.ObjectMeta.Name, s.k8sClient)
+//	g.Expect(kubeconfig).Should(BeNil())
+//	g.Expect(err).ShouldNot(BeNil())
+//	g.Expect(apierrors.IsNotFound(err)).Should(BeFalse())
+//}
+//
+//func (s *ClientTestSuite) TestExtractKubeConfigFromSecret(t *testing.T) {
+//	g := NewWithT(t)
+//	cleanup := setupGetKubeconfigTest(t, s.k8sClient)
+//	defer cleanup()
+//	kubeconfigBuffer, err := readFile(kubeConfigPath)
+//	g.Expect(err).Should(BeNil())
+//	kubeconfig := kubeconfigBuffer.Bytes()
+//	g.Expect(kubeconfig).ShouldNot(BeNil())
+//
+//	secret.Data = map[string][]byte{
+//		"kubeconfig": kubeconfig,
+//	}
+//	err = s.k8sClient.Create(ctx, &secret)
+//	g.Expect(err).Should(BeNil())
+//
+//	actualKubeconfig, err := GetKubeConfigFromSecret(ctx, secret.ObjectMeta.Namespace, secret.ObjectMeta.Name, s.k8sClient)
+//	g.Expect(err).Should(BeNil())
+//	g.Expect(actualKubeconfig).Should(Equal(kubeconfig))
+//}
+//
+//func (s *ClientTestSuite) TestDeploymentNotFound(t *testing.T) {
+//	g := NewWithT(t)
+//	setupGetDeploymentTest(t)
+//	actual, err := GetDeploymentFor(ctx, deployment.ObjectMeta.Namespace, deployment.ObjectMeta.Name, s.k8sClient)
+//	g.Expect(apierrors.IsNotFound(err)).Should(BeTrue())
+//	g.Expect(actual).Should(BeNil())
+//}
+//
+//func (s *ClientTestSuite) TestFoundDeployment(t *testing.T) {
+//	g := NewWithT(t)
+//	setupGetDeploymentTest(t)
+//
+//	err := s.k8sClient.Create(ctx, &deployment)
+//	g.Expect(err).Should(BeNil())
+//
+//	actual, err := GetDeploymentFor(ctx, deployment.ObjectMeta.Namespace, deployment.ObjectMeta.Name, s.k8sClient)
+//	g.Expect(err).Should(BeNil())
+//	g.Expect(actual).ShouldNot(BeNil())
+//	g.Expect(actual.ObjectMeta.Name).Should(Equal(deployment.ObjectMeta.Name))
+//	g.Expect(actual.ObjectMeta.Namespace).Should(Equal(deployment.ObjectMeta.Namespace))
+//
+//	err = s.k8sClient.Delete(ctx, &deployment)
+//	g.Expect(err).Should(BeNil())
+//}
+//
+//func (s *ClientTestSuite) TestCreateScalesGetter(t *testing.T) {
+//	g := NewWithT(t)
+//	scaleGetter, err := CreateScalesGetter(s.cfg)
+//	g.Expect(err).Should(BeNil())
+//	g.Expect(scaleGetter).ShouldNot(BeNil())
+//}
+//
+//func (s *ClientTestSuite) TestCreateClientFromKubeconfigBytes(t *testing.T) {
+//	g := NewWithT(t)
+//	kubeconfigBuffer, err := readFile(kubeConfigPath)
+//	g.Expect(err).Should(BeNil())
+//	kubeconfig := kubeconfigBuffer.Bytes()
+//	g.Expect(kubeconfig).ShouldNot(BeNil())
+//
+//	cfg, err := CreateClientFromKubeConfigBytes(kubeconfig)
+//	g.Expect(err).Should(BeNil())
+//	g.Expect(cfg).ShouldNot(BeNil())
+//}
+//
+//// func setupEnvSuite() func() {
+//// 	log.Println("setting up testEnv")
+//// 	fileExistsOrFail(secretPath)
+//// 	fileExistsOrFail(deploymentPath)
+//// 	fileExistsOrFail(kubeConfigPath)
+//// 	testEnv = &envtest.Environment{}
+//// 	cfg, err = testEnv.Start()
+//// 	if err != nil {
+//// 		log.Fatalf("error in starting testEnv: %v", err)
+//// 	}
+//// 	if cfg == nil {
+//// 		log.Fatalf("Got nil config from testEnv")
+//// 	}
+//// 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+//// 	if err != nil {
+//// 		log.Fatalf("error in creating new client: %v", err)
+//// 	}
+//// 	if k8sClient == nil {
+//// 		log.Fatalf("Got a nil k8sClient")
+//// 	}
+//// 	return func() {
+//// 		log.Println("tearing down envTest")
+//// 		err := testEnv.Stop()
+//// 		if err != nil {
+//// 			log.Fatalf("error in stopping testEnv: %v", err)
+//// 		}
+//// 	}
+//// }
+//
 func setupGetKubeconfigTest(t *testing.T, k8sClient client.Client) func() {
 	g := NewWithT(t)
 	ctx = context.Background()
@@ -203,15 +235,16 @@ func setupGetKubeconfigTest(t *testing.T, k8sClient client.Client) func() {
 	}
 }
 
-func setupGetDeploymentTest(t *testing.T) {
-	g := NewWithT(t)
-	ctx = context.Background()
-	result := getStructured[appsv1.Deployment](deploymentPath)
-	g.Expect(result.Err).Should(BeNil())
-	g.Expect(result.StructuredObject).ShouldNot(BeNil())
-	deployment = result.StructuredObject
-}
-
+//
+//func setupGetDeploymentTest(t *testing.T) {
+//	g := NewWithT(t)
+//	ctx = context.Background()
+//	result := getStructured[appsv1.Deployment](deploymentPath)
+//	g.Expect(result.Err).Should(BeNil())
+//	g.Expect(result.StructuredObject).ShouldNot(BeNil())
+//	deployment = result.StructuredObject
+//}
+//
 type Result[T any] struct {
 	StructuredObject T
 	Err              error
@@ -222,13 +255,11 @@ func getStructured[T any](filepath string) Result[T] {
 	if err != nil {
 		return Result[T]{Err: err}
 	}
-
 	var structuredObject T
 	err = runtime.DefaultUnstructuredConverter.FromUnstructured(unstructuredObject.Object, &structuredObject)
 	if err != nil {
 		return Result[T]{Err: err}
 	}
-
 	return Result[T]{StructuredObject: structuredObject}
 }
 
@@ -237,7 +268,6 @@ func getUnstructured(filePath string) (*unstructured.Unstructured, error) {
 	if err != nil {
 		return &unstructured.Unstructured{}, err
 	}
-
 	jsonObject, err := yaml.ToJSON(buff.Bytes())
 	if err != nil {
 		return &unstructured.Unstructured{}, err
@@ -247,12 +277,10 @@ func getUnstructured(filePath string) (*unstructured.Unstructured, error) {
 	if err != nil {
 		return &unstructured.Unstructured{}, err
 	}
-
 	unstructuredObject, ok := object.(*unstructured.Unstructured)
 	if !ok {
 		return &unstructured.Unstructured{}, fmt.Errorf("unstructured.Unstructured expected")
 	}
-
 	return unstructuredObject, nil
 }
 
