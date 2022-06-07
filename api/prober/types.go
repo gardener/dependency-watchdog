261b48b8cb55cb14abfa141f22d911452675c362
Madhav Bhargava <madhav.bhargava@sap.com>
2022-06-07 09:16:40 +0530

    added comments

--- api/prober/types.go
+++ api/prober/types.go
@@ -6,27 +6,43 @@ import (
 	autoscalingv1 "k8s.io/api/autoscaling/v1"
 )
 
+// Config provides typed access to prober configuration
 type Config struct {
-	InternalKubeConfigSecretName        string                  `yaml:"internalKubeConfigSecretName"`
-	ExternalKubeConfigSecretName        string                  `yaml:"externalKubeConfigSecretName"`
-	ProbeInterval                       *time.Duration          `yaml:"probeInterval,omitempty"`
-	InitialDelay                        *time.Duration          `yaml:"initialDelay,omitempty"`
-	ProbeTimeout                        *time.Duration          `yaml:"probeTimeout,omitempty"`
-	SuccessThreshold                    *int                    `yaml:"successThreshold,omitempty"`
-	FailureThreshold                    *int                    `yaml:"failureThreshold,omitempty"`
-	InternalProbeFailureBackoffDuration *time.Duration          `yaml:"internalProbeFailureBackoffDuration,omitempty"`
-	BackoffJitterFactor                 *float64                `yaml:"backoffJitterFactor,omitempty"`
-	DependentResourceInfos              []DependentResourceInfo `yaml:"dependentResourceInfos"`
+	// InternalKubeConfigSecretName is the name of the kubernetes secret which has the kubeconfig to connect to the shoot control plane API server via internal domain
+	InternalKubeConfigSecretName string `yaml:"internalKubeConfigSecretName"`
+	// ExternalKubeConfigSecretName is the name of the kubernetes secret which has the kubeconfig to connect to the shoot control plane API server via external domain
+	ExternalKubeConfigSecretName string `yaml:"externalKubeConfigSecretName"`
+	// ProbeInterval is the interval with which the probe will be run
+	ProbeInterval *time.Duration `yaml:"probeInterval,omitempty"`
+	// InitialDelay is the initial delay in running a probe for the first time
+	InitialDelay *time.Duration `yaml:"initialDelay,omitempty"`
+	// ProbeTimeout is the timeout that is set on the client which is used to reach the shoot control plane API server
+	ProbeTimeout *time.Duration `yaml:"probeTimeout,omitempty"`
+	// SuccessThreshold is the number of consecutive times a probe is successful to ascertain that the probe is healthy
+	SuccessThreshold *int `yaml:"successThreshold,omitempty"`
+	// FailureThreshold is the number of consecutive times a probe is unsuccessful to ascertain that the probe is unhealthy
+	FailureThreshold *int `yaml:"failureThreshold,omitempty"`
+	// InternalProbeFailureBackoffDuration backoff duration if the internal probe is unhealthy, before attempting again
+	InternalProbeFailureBackoffDuration *time.Duration `yaml:"internalProbeFailureBackoffDuration,omitempty"`
+	// BackoffJitterFactor jitter with which a probe is run
+	BackoffJitterFactor *float64 `yaml:"backoffJitterFactor,omitempty"`
+	// DependentResourceInfos dependent resources that should be considered for scaling in case the shoot control API server cannot be reached via external domain
+	DependentResourceInfos []DependentResourceInfo `yaml:"dependentResourceInfos"`
 }
 
+// DependentResourceInfo captures a dependent resource which should be scaled
 type DependentResourceInfo struct {
 	// Ref identifies a resource
-	Ref           *autoscalingv1.CrossVersionObjectReference `yaml:"ref"`
-	ShouldExist   *bool                                      `yaml:"shouldExist"`
-	ScaleUpInfo   *ScaleInfo                                 `yaml:"scaleUp,omitempty"`
-	ScaleDownInfo *ScaleInfo                                 `yaml:"scaleDown,omitempty"`
+	Ref *autoscalingv1.CrossVersionObjectReference `yaml:"ref"`
+	// ShouldExist should be true if this resource should be present. If the resource is optional then it should be false.
+	ShouldExist *bool `yaml:"shouldExist"`
+	// ScaleUpInfo captures the configuration to scale up the resource identified by Ref
+	ScaleUpInfo *ScaleInfo `yaml:"scaleUp,omitempty"`
+	// ScaleDownInfo captures the configuration to scale down the resource identified by Ref
+	ScaleDownInfo *ScaleInfo `yaml:"scaleDown,omitempty"`
 }
 
+// ScaleInfo captures the configuration required to scale a dependent resource
 type ScaleInfo struct {
 	// Level is used to order the dependent resources. Highest level or the first level starts at 0 and increments. Each dependent resource on a level will have to wait for
 	// all resource in a previous level to finish their scaling operation. If there are more than one resource defined with the same level then they will be scaled concurrently.
