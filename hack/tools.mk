fc34de48b9515fb0df98e96b537e4db2a9c8814e
Rafael Franzke <rafael.franzke@sap.com>
2024-03-14 14:31:38 +0100

    [prober] Skip `Lease`s belonging to non-existing `Node`s (#106)
    * Exclude `Lease`s belonging to non-existing `Node`s
    
    * Adapt unit tests
    
    * Add unit test for new code
    
    * Pin `logcheck` version to `v1.86.0`
    
    This is the same version of `gardener/gardener` as used in `go.mod`.
    
    See https://prow.gardener.cloud/view/gs/gardener-prow/pr-logs/pull/gardener_dependency-watchdog/106/pull-dependency-watchdog-unit/1767951173676961792 for motivation
    
    * Address PR review feedback
    
    * Address PR review feedback

--- hack/tools.mk
+++ hack/tools.mk
@@ -15,7 +15,7 @@ SETUP_ENVTEST     := $(TOOLS_BIN_DIR)/setup-envtest
 GOLANGCI_LINT_VERSION ?= v1.55.2
 GO_VULN_CHECK_VERSION ?= latest
 GOIMPORTS_VERSION ?= latest
-LOGCHECK_VERSION ?= latest
+LOGCHECK_VERSION ?= cee120166b7b8a857dea90fc0217d86c2e41850f # this commit hash corresponds to v1.86.0 which is the gardener/gardener version in go.mod - we could use regular tags when https://github.com/gardener/gardener/issues/8811 is resolved
 GO_ADD_LICENSE_VERSION ?= latest
 # import boss failing with latest , so pinning to most up-to-date successfull version
 # refer https://pkg.go.dev/k8s.io/code-generator@v0.26.3/cmd/import-boss?tab=versions for list of versions
@@ -37,7 +37,6 @@ $(GOLANGCI_LINT):
 
 $(GOIMPORTS):
 	GOBIN=$(abspath $(TOOLS_BIN_DIR)) go install golang.org/x/tools/cmd/goimports@$(GOIMPORTS_VERSION)
-
 
 $(LOGCHECK):
 	GOBIN=$(abspath $(TOOLS_BIN_DIR)) go install github.com/gardener/gardener/hack/tools/logcheck@$(LOGCHECK_VERSION)
